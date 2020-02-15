40e590614229365863a5bb8063bc2565115833c8
Konstantin Zhidovinov <kzhidovinov@gmail.com>
2020-02-15 16:32:23 +0300

    Added logger

--- ApiServer/Program.cs
+++ ApiServer/Program.cs
@@ -2,24 +2,27 @@
 {
     using System.IO;
     using System.Reflection;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             new HostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureHostConfiguration(configurationBuilder => configurationBuilder
-                .AddEnvironmentVariables(prefix: "DOTNET_"))
+                .ConfigureHostConfiguration(configurationBuilder => configurationBuilder.AddEnvironmentVariables(prefix: "DOTNET_"))
+                .ConfigureLogging(logger =>
+                    {
+                        logger.ClearProviders();
+                        logger.AddConsole();
+                    })
                 .ConfigureAppConfiguration((hostingContext, config) => config
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
                     .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",
@@ -43,6 +46,7 @@
                 .UseIIS()
                 .ConfigureServices(services => services
                     .Configure<IISServerOptions>(config => config.AllowSynchronousIO = true))
-                .UseStartup<Startup>());
+                .UseStartup<Startup>())
+                .UseConsoleLifetime();
     }
 }
--- ApiServer/Startup.cs
+++ ApiServer/Startup.cs
@@ -44,6 +44,8 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.If(env.EnvironmentName == "Development", app => app.UseDeveloperExceptionPage());
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
