1557fd1905a2172e6bbadcbbb5481635cd219021
Konstantin Zhidovinov <kzhidovinov@gmail.com>
2020-02-26 10:07:17 +0300

    Added WayShapes view to DB.

--- Source/DataAccessLayer/Migrations/20200226064833_AddWayShapesView.Designer.cs
+++ Source/DataAccessLayer/Migrations/20200226064833_AddWayShapesView.Designer.cs
@@ -4,15 +4,17 @@ using GisApi.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(SqlServerDbContext))]
-    partial class SqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200226064833_AddWayShapesView")]
+    partial class AddWayShapesView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/DataAccessLayer/Migrations/20200226064833_AddWayShapesView.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AddWayShapesView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE VIEW [dbo].[WayShapes]
+AS
+WITH data AS (
+    SELECT
+        d.Id,
+        string_agg(
+            CAST(d.point.STX AS nvarchar(50)) + ' ' + CAST(d.point.STY AS nvarchar(50)),
+            ','
+        ) WITHIN GROUP (
+            ORDER BY
+                d.WayIdx
+        ) AS wkt
+    FROM
+        (
+            SELECT
+                w.Id,
+                wn.WayIdx,
+                n.Location AS point
+            FROM
+                Ways w
+                JOIN WayNodes wn ON wn.WayId = w.Id
+                JOIN Nodes n ON wn.NodeId = n.Id
+        ) AS d
+    GROUP BY
+        d.Id
+    HAVING
+        COUNT(d.point) > 1
+)
+SELECT
+    g.Id,
+    geometry :: STGeomFromText('LINESTRING(' + g.wkt + ')', 4326) AS shape
+FROM
+    data g;
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW [dbo].[WayShapes]");
+        }
+    }
+}
